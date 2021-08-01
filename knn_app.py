0fa4581045203ab284ab12611bebb3b7c8a0b7a0
Soham Naha <37201714+soham2109@users.noreply.github.com>
2021-08-01 12:13:08 +0530

    Update knn_app.py

--- knn_app.py
+++ knn_app.py
@@ -5,10 +5,10 @@ If streamlit is not installed use, pip install streamlit
 The app opens in the browser at localhost:8501
 """
 import SMA_algotrad
-import k-nearest-neighbours
+import k_nearest_neighbours
 import streamlit as st
 
-PAGES = {"KNN"neighbours: k-nearest-neighbours,
+PAGES = {"KNN"neighbours: k_nearest_neighbours,
 		 "Simple Moving Average": SMA_algotrad}
 
 def app():
